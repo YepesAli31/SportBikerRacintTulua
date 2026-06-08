a01016b865242d8697dbcfb72817e26170c1eab9
YepesAli31 <jeyepes75@gmail.com>
2026-06-07 19:04:57 -0500

    Add files via upload

--- productos.js
+++ productos.js
@@ -5,28 +5,28 @@ const productosData = [
         nombre: "Suspensión Delantera RockShox Boxxer 29",
         precio: 4200000,
         imagen: "suspension29.jpg",
-        categoria: "downhill"
+        categoria: "downhill",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Frenos Hidráulicos Shimano Saint M820 (Par)",
         precio: 1850000,
         imagen: "frenosaint.jpg",
-        categoria: "downhill"
+        categoria: "downhill",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Llanta Maxxis Assegai 29x2.50 3C Downhill",
         precio: 3400000,
         imagen: "maxxis.jpg",
-        categoria: "downhill"
+        categoria: "downhill",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Manubrio Renthal Fatbar Carbon 35mm",
         precio: 720000,
         imagen: "renthal.jpg",
-        categoria: "downhill"
+        categoria: "downhill",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
 
@@ -35,28 +35,28 @@ const productosData = [
         nombre: "Marco BMX Sunday Soundwave V3 21\"",
         precio: 1950000,
         imagen: "bmxsund.jpg",
-        categoria: "bmx"
+        categoria: "bmx",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Bielas BMX Odyssey Thunderbolt RHD",
         precio: 850000,
         imagen: "bielas.jpg",
-        categoria: "bmx"
+        categoria: "bmx",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Manzana Trasera Casera Odyssey Clutch V2",
         precio: 680000,
         imagen: "idyman.jpg",
-        categoria: "bmx"
+        categoria: "bmx",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Pedales Odyssey Twisted PC",
         precio: 120000,
         imagen: "pedales.jpg",
-        categoria: "bmx"
+        categoria: "bmx",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
 
@@ -65,28 +65,28 @@ const productosData = [
         nombre: "Grupo Shimano Ultegra Di2 R8100 12v",
         precio: 6500000,
         imagen: "ultegra.jpg",
-        categoria: "ruta"
+        categoria: "ruta",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Rines de Carbono Shimano C50 Ruta (Par)",
         precio: 4800000,
         imagen: "rines.jpg",
-        categoria: "ruta"
+        categoria: "ruta",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Llanta Continental Grand Prix 5000 700x25c",
         precio: 320000,
         imagen: "grand.jpg",
-        categoria: "ruta"
+        categoria: "ruta",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Sillín Prologo Dimension NDR Tirox",
         precio: 450000,
         imagen: "sillim.jpg",
-        categoria: "ruta"
+        categoria: "ruta",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
 
@@ -95,28 +95,28 @@ const productosData = [
         nombre: "Cadena Shimano KMC X12 Oro/Negro",
         precio: 180000,
         imagen: "cadena.jpg",
-        categoria: "componentes"
+        categoria: "componentes",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Pastillas de Freno Shimano Resina H03A",
         precio: 950000,
         imagen: "pastillas.jpg",
-        categoria: "componentes"
+        categoria: "componentes",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Extractor de Cassette Park Tool FR-5.2",
         precio: 65000,
         imagen: "extra.jpg",
-        categoria: "componentes"
+        categoria: "componentes",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     },
     {
         nombre: "Líquido de Frenos Mineral Shimano 100ml",
         precio: 35000,
         imagen: "liqui.jpg",
-        categoria: "componentes"
+        categoria: "componentes",
         linkPago: "https://link.mercadopago.com.co/sporbikeracing"
     }
 ];
